6a8c85f6dcad9d1c5cfe38999d3759db6d205a5d
Yehonal <hw.2@hotmail.it>
2015-06-10 14:02:43 +0200

    update all repository after message changing

--- tests/class.js
+++ tests/class.js
@@ -3,7 +3,7 @@
  * GNU General Public License version 3; see www.hyperweb2.com/terms/
  */
 
-hw2.defTests(function () {
+hwc.defTests(function () {
     $ = this;
 
     describe('Class', function () {
