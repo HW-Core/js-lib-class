9fb9ae8a7308e355dd16954ced50bfb284d8df2e
Yehonal <hw.2@hotmail.it>
2016-07-18 19:57:39 +0200

    fixes to installation script

--- install.sh
+++ install.sh
@@ -11,17 +11,17 @@ function add_repo() {
     path=$DIR/$basedir/$name
     
     (git --git-dir=$path/.git/ rev-parse && git --git-dir=$path/.git/ pull origin $branch) || git clone $url/$name.git -b $branch $path 
-	[ -f $DIR/$3/$1/install.sh ] && bash $path/install.sh $params
+	[ -f $path/install.sh ] && bash $path/install.sh $params
 }
 
 function add_file() {
     mkdir -p $2
-    wget -nc $1 -P $2
+    wget -nc $1 -P $DIR/$2
 }
 
 #
 # ADD DEPENDENCIES
 #
 
-add_repo "js-kernel" "master" "../"
+add_repo "git@github.com:HW-Core" "js-kernel" "master" "../"
 
