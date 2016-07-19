ce9a032a34c5f5017d2502e2807e9f3a6dbea1a6
Yehonal <hw.2@hotmail.it>
2016-07-19 08:57:45 +0200

    fixed install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-PATH_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../../"
-[ ! -d $PATH_ROOT ] && git clone https://github.com/udw/joiner $PATH_ROOT/udw/joiner -b master
-source "$PATH_ROOT/udw/joiner/joiner.sh"
+PATH_MODULES="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../../"
+[ ! -d $PATH_MODULES/udw/joiner ] && git clone https://github.com/udw/joiner $PATH_MODULES/udw/joiner -b master
+source "$PATH_MODULES/udw/joiner/joiner.sh"
 
 
 #
 # ADD DEPENDENCIES
 #
 
-add_repo "https://github.com/HW-Core" "js-kernel" "master" "../"
+Joiner:add_repo "https://github.com/HW-Core" "js-kernel" "master" "../"
 
