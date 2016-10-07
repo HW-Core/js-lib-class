21fd2e1b9379ef382dc63d44249b2a02f04f2e4b
Yehonal <yehonal.azeroth@gmail.com>
2016-10-07 18:55:28 +0200

    using more compatible path for bash

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 PATH_MODULES="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../../"
 [ ! -d $PATH_MODULES/uwd/joiner ] && git clone https://github.com/uw-dev/joiner $PATH_MODULES/uwd/joiner -b master
